3b6369c52b6074261ac2544b1cd70b15f12315a0
salavatn <salavat@nigmatullin.net>
2022-06-11 23:26:42 +0500

    Lab. Project5. Task - create array

--- Project5.py
+++ Project5.py
@@ -1,14 +1,10 @@
-auto = [["Mitsubishi Outlander", "Mitsubishi Pajero Sport", "Mitsubishi ASX"]]
-auto.append(["Renault Arkana", "Renault Duster", "Renault Kaptur"])
-auto.append(["BMW X1","BMW X2", "BMW X3", "BMW X6"])
-auto.insert(0,"MAZDA")
-auto.insert(2,["Kia K5", "Kia K9"])
+userLength = int(input("Enter length of array: "))
+userArray = []
+userCounter = 0
 
-carAudi = [["Audi A1","Audi A2","Audi A3","Audi A4"]]
-auto.extend(carAudi)
-print("List of car:", auto)
+while userCounter < userLength:
+    msg = "Enter element #" + str(userCounter + 1) + ": "
+    userArray.append(input(msg))
+    userCounter += 1
 
-print("\n\n")
-
-for X in auto:
-    print("Elements of array = ",X)
+print("New array created: ",userArray)
