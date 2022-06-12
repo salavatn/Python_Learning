82e6193ebdaa383b152f550ad205eacf2cad1343
salavatn <salavat@nigmatullin.net>
2022-06-12 12:31:57 +0500

    Lab. Project6. Функции строк. Индексы and string

---
+++ Lab-1.py
@@ -0,0 +1,10 @@
+word = "Football"
+print(word[0:4])    # Output is: Foot
+print(word[4])      # Output is: b
+print(word[4:9])    # Output is: ball
+print(word[4:])     # Output is: ball
+
+print(word[0:9:1])  # Output is: Football
+print(word[0:9:2])  # Output is: Fobl
+print(word[0:9:3])  # Output is: Ftl
+print(word[0:9:4])  # Output is: Fb
