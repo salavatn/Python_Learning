e4fe91a89d1ec8acc4d98b59aa3196e64b8c7b6b
salavatn <salavat@nigmatullin.net>
2022-06-24 15:22:23 +0500

    work with SQLAlchemy, Request (POST/GET), Regirect

--- app.py
+++ app.py
@@ -1,24 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
-    # return "Set your NAME and ID in URL: http://176.213.150.93/NAME/ID/"
-
 
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
+@app.route('/create-article',methods=['POST','GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title,intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "ERROR: The message is not uploaded!"
+
+    else:
+        return render_template("create-article.html")
+
+
 @app.route('/checkout')
 def checkout():
     return render_template("checkout.html")
 
 
-@app.route('/<string:user_name>/<int:user_id>/')
-def user_info(user_name,user_id):
-    return "Hello, " + user_name + "! Your ID:" + str(user_id)
